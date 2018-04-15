ab768fa053a639420001424c33f84725f2f663ed
Kyou13 <kyou1013@gmail.com>
2018-04-16 00:58:03 +0900

    fix bg

---
+++ crawls/static/crawls/js/top.js
@@ -0,0 +1,23 @@
+var th_title = document.getElementById("th-title");
+var th_address = document.getElementById("th-address");
+var th_date = document.getElementById("th-date");
+
+var tr_title = document.getElementById('td-title');
+var tr_address = document.getElementById('td-address');
+var tr_date = document.getElementById('td-date');
+
+th_title.width = tr_title.clientWidth;
+th_address.width = tr_address.clientWidth;
+th_date.width = tr_date.clientWidth;
+
+// 背景
+$('#bg').particleground({ //要素の指定
+    dotColor:  '#7dfcfd', //ドットの色
+    lineColor: '#7dfcfd',
+    density: 6500,
+    minSpeedX: 0.2,
+    particleRadius: 5,
+    parallax: false,
+    maxSpeedX: 0.1,
+    maxSpeedY: 0.1,
+});
