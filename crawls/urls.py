0e40ea417d7482c4323139702e1bc05a6333fc91
Kyou13 <kyou1013@gmail.com>
2018-04-16 01:22:38 +0900

    fix bg

--- crawls/urls.py
+++ crawls/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('',views.top,name="top"),
     path('<int:year>-<int:month>/',views.top,name="top"),
-    path('api/',views.crawl,name="out_csv"),
+    path('api/<int:year>-<int:month>/',views.crawl,name="out_csv"),
     # path('add/',views.crawl_edit,name="crawl_add"),
     path('list/',views.crawl_list,name="crawl_list"),
     # path('<int:crawl_id>/', views.crawl_detail, name='crawl_detail'),
